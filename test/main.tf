a167ec10e59c1093662ed9e011fd1c0b66a35a0d
remche <remche@remche.org>
2021-06-01 22:41:46 +0200

    add tests

---
+++ test/main.tf
@@ -0,0 +1,37 @@
+module "controlplane" {
+  source = "./.."
+  dns_domain         = "u-ga.fr."
+  dns_servers        = ["152.77.1.22", "195.83.24.30"]
+
+
+  image_name      = "ubuntu-20.04-focal-x86_64+rke2"
+  public_net_name = "public"
+  secgroup_rules = [{ "source" = "152.77.119.207/32", "protocol" = "tcp", "port" = 22 },
+  { "source" = "152.77.119.207/32", "protocol" = "icmp", port = 0 },
+  { "source" = "147.171.168.176/32", "protocol" = "icmp", port = 0 },
+  { "source" = "152.77.119.207/32", "protocol" = "tcp", "port" = 6443 },
+  { "source" = "147.171.168.176/32", "protocol" = "tcp", "port" = 22 },
+  { "source" = "147.171.168.176/32", "protocol" = "tcp", "port" = 6443 },
+  { "source" = "0.0.0.0/0", "protocol" = "tcp", "port" = 80 },
+  { "source" = "0.0.0.0/0", "protocol" = "tcp", "port" = 443 }
+  ]
+  master_flavor_name = "m1.small"
+}
+
+module "blue_node" {
+  source = "./..//modules/worker"
+  image_name      = "ubuntu-20.04-focal-x86_64+rke2"
+  nodes_count = 1
+  name_prefix = "blue"
+  flavor_name = "m1.small"
+  node_config = module.controlplane.node_config
+}
+
+module "green_node" {
+  source = "./..//modules/worker"
+  image_name      = "ubuntu-20.04-focal-x86_64+rke2"
+  nodes_count = 1
+  name_prefix = "green"
+  flavor_name = "m1.small"
+  node_config = module.controlplane.node_config
+}
