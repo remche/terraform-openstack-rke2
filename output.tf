d6b8f9bb8e28fd7dc59e5032a66fa89f4481d362
remche <remche@remche.org>
2021-06-02 16:40:59 +0200

    output

--- output.tf
+++ output.tf
@@ -4,16 +4,6 @@ output "keypair_name" {
   sensitive   = "true"
 }
 
-output "master_nodes" {
-  value       = module.master.nodes
-  description = "The master nodes"
-}
-
-output "nodes_subnet" {
-  value       = module.network.nodes_subnet
-  description = "The nodes subnet"
-}
-
 output "node_config" {
   value = local.node_config
   sensitive = true
