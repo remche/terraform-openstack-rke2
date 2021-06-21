dd719407524ff7fcc4a290bdca620dab56c1edf4
remche <remche@remche.org>
2021-06-21 17:02:47 +0200

    fmt examples

--- examples/blue-green-nodes/outputs.tf
+++ examples/blue-green-nodes/outputs.tf
@@ -1,5 +1,5 @@
 output "server_ip" {
- description = "Server floating IP"
- value       = module.controlplane.floating_ip[0]
- sensitive   = true
+  description = "Server floating IP"
+  value       = module.controlplane.floating_ip[0]
+  sensitive   = true
 }
