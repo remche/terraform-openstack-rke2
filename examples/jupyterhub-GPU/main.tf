ffe9acee09bb2c62362bde27036a94f807ecaf41
yashani <85614514+yashani@users.noreply.github.com>
2021-12-09 17:42:06 +0100

    Update main.tf

--- examples/jupyterhub-GPU/main.tf
+++ examples/jupyterhub-GPU/main.tf
@@ -1,8 +1,9 @@
 module "controlplane" {
-  source           = "remche/rke2/openstack"
+  source           = "yashani/rke2/openstack"
   write_kubeconfig = true
-  image_name       = "ubuntu-20.04-focal-x86_64"
-  flavor_name      = "m1.small"
+  image_name       = "ubuntu-20.04-docker-g4-cudnn-x86_64"
+  flavor_name      = "m1.large-2d"
+  dns_servers      = ["193.48.86.103", "193.48.86.224"]
   public_net_name  = "public"
   rke2_config_file = "server.yaml"
   manifests_path   = "./manifests"
@@ -11,10 +12,10 @@ module "controlplane" {
 }
 
 module "worker" {
-  source           = "remche/rke2/openstack//modules/agent"
-  image_name       = "ubuntu-20.04-focal-x86_64"
+  source           = "yashani/rke2/openstack//modules/agent"
+  image_name       = "ubuntu-20.04-docker-g4-cudnn-x86_64"
   nodes_count      = 2
   name_prefix      = "worker"
-  flavor_name      = "m1.small"
+  flavor_name      = "g4.xlarge-4xmem"
   node_config      = module.controlplane.node_config
 }
