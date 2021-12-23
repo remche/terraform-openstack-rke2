5406af249de32815b36fe0daa0e0f5c243ddb548
remche <remche@remche.org>
2021-12-23 11:01:26 +0100

    fmt jupyter-gpu example

--- examples/jupyterhub-gpu/main.tf
+++ examples/jupyterhub-gpu/main.tf
@@ -7,14 +7,14 @@ module "controlplane" {
   rke2_config_file = "server.yaml"
   manifests_path   = "./manifests"
   # Fix for https://github.com/rancher/rke2/issues/1113
-  additional_san   = ["kubernetes.default.svc"]
+  additional_san = ["kubernetes.default.svc"]
 }
 
 module "worker" {
-  source           = "remche/rke2/openstack//modules/agent"
-  image_name       = "fg-services-ubuntu-20.04-x86_64.raw"
-  nodes_count      = 1
-  name_prefix      = "worker"
-  flavor_name      = "g4.xlarge-4xmem"
-  node_config      = module.controlplane.node_config
+  source      = "remche/rke2/openstack//modules/agent"
+  image_name  = "fg-services-ubuntu-20.04-x86_64.raw"
+  nodes_count = 1
+  name_prefix = "worker"
+  flavor_name = "g4.xlarge-4xmem"
+  node_config = module.controlplane.node_config
 }
