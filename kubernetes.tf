4a56cbdd33d861ba2409fd5aba322c794d06c943
remche <remche@remche.org>
2022-05-31 22:03:00 +0200

    pin Invicton-Labs/shell-resource version

--- kubernetes.tf
+++ kubernetes.tf
@@ -19,6 +19,7 @@ resource "null_resource" "write_kubeconfig" {
 
 module "host" {
   source       = "Invicton-Labs/shell-resource/external"
+  version      = "0.3.3"
   count        = var.output_kubernetes_config ? 1 : 0
   command_unix = "${local.ssh} ${var.system_user}@${module.server.floating_ip[0]} sudo KUBECONFIG=/etc/rancher/rke2/rke2-remote.yaml /var/lib/rancher/rke2/bin/kubectl config view --raw=true -o jsonpath='{.clusters[0].cluster.server}'"
   depends_on = [
@@ -28,6 +29,7 @@ module "host" {
 
 module "client_certificate" {
   source       = "Invicton-Labs/shell-resource/external"
+  version      = "0.3.3"
   count        = var.output_kubernetes_config ? 1 : 0
   command_unix = "${local.ssh} ${var.system_user}@${module.server.floating_ip[0]} sudo KUBECONFIG=/etc/rancher/rke2/rke2-remote.yaml /var/lib/rancher/rke2/bin/kubectl config view --raw=true -o jsonpath='{.users[0].user.client-certificate-data}'"
   depends_on = [
@@ -37,6 +39,7 @@ module "client_certificate" {
 
 module "client_key" {
   source       = "Invicton-Labs/shell-resource/external"
+  version      = "0.3.3"
   count        = var.output_kubernetes_config ? 1 : 0
   command_unix = "${local.ssh} ${var.system_user}@${module.server.floating_ip[0]} sudo KUBECONFIG=/etc/rancher/rke2/rke2-remote.yaml /var/lib/rancher/rke2/bin/kubectl config view --raw=true -o jsonpath='{.users[0].user.client-key-data}'"
   depends_on = [
@@ -46,6 +49,7 @@ module "client_key" {
 
 module "cluster_ca_certificate" {
   source       = "Invicton-Labs/shell-resource/external"
+  version      = "0.3.3"
   count        = var.output_kubernetes_config ? 1 : 0
   command_unix = "${local.ssh} ${var.system_user}@${module.server.floating_ip[0]} sudo KUBECONFIG=/etc/rancher/rke2/rke2-remote.yaml /var/lib/rancher/rke2/bin/kubectl config view --raw=true -o jsonpath='{.clusters[0].cluster.certificate-authority-data}'"
   depends_on = [
