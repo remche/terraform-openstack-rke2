f17f649510fccbd50425b6ef1e7ae9011fa58b65
remche <remche@remche.org>
2023-03-01 10:53:38 +0100

    bump simple example to genX2

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -3,6 +3,6 @@ module "controlplane" {
   cluster_name     = var.cluster_name
   write_kubeconfig = true
   image_name       = "ubuntu-20.04-focal-x86_64"
-  flavor_name      = "genX1"
+  flavor_name      = "genX2"
   public_net_name  = "dmz"
 }
