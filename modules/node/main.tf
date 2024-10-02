db06b0792524c01ce56f59ec91c16846c7b47fe7
remche <remche@remche.org>
2024-10-02 10:55:36 +0200

    use openstack_networking_floatingip_associate_v2. Fix #70

--- modules/node/main.tf
+++ modules/node/main.tf
@@ -96,10 +96,10 @@ resource "openstack_networking_floatingip_v2" "floating_ip" {
   pool  = var.floating_ip_pool
 }
 
-resource "openstack_compute_floatingip_associate_v2" "associate_floating_ip" {
+resource "openstack_networking_floatingip_associate_v2" "associate_floating_ip" {
   count       = var.assign_floating_ip ? var.nodes_count : 0
   floating_ip = openstack_networking_floatingip_v2.floating_ip[count.index].address
-  instance_id = openstack_compute_instance_v2.instance[count.index].id
+  port_id     = openstack_networking_port_v2.port[count.index].id
 }
 
 resource "null_resource" "upgrade" {
--- modules/node/output.tf
+++ modules/node/output.tf
@@ -1,5 +1,5 @@
 output "floating_ip" {
-  value = openstack_compute_floatingip_associate_v2.associate_floating_ip[*].floating_ip
+  value = openstack_networking_floatingip_associate_v2.associate_floating_ip[*].floating_ip
 }
 
 output "internal_ip" {
